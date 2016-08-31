745fa9d6275162e1954878f8b52f171d8f22a120
falldj <derek.isensee@gmail.com>
2016-08-30 22:11:49 -0500

    everything is all fixed up
    new player creation works. need to set up individual player editing.

--- index.php
+++ index.php
@@ -17,13 +17,32 @@
         <p>Welcome to the place where the best domino players are on display</p>
         <p>Enter a name to find the player of your interest.</p>
         <form action="processData.php" method="post">
-            <input type="text" name="name">
-            <input type="text" name="wins">
-            <input type="text" name="losses">
+            Name: <input type="text" name="name">
+            Wins: <input type="text" name="wins">
+            Losses: <input type="text" name="losses">
+            # of Times in Yard: <input type="text" name="yard">
             <input type="submit">
         </form>
     <p> Or click here to make a new player</p>
-    <a href="page.html"><button>Create</button></a>
+        
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "players";
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+    $result = mysqli_query($con, "SELECT * FROM player");
+        
+    while ($row = mysqli_fetch_assoc($result)) 
+    {
+        echo "<strong>Name: </strong>" . $row['name'];
+        echo " <strong>Wins: </strong>" . $row['wins'];
+        echo " <strong>Losses: </strong>" . $row['losses'];
+        echo " <strong>Times In Yard: </strong>" . $row['timesInYard'];
+        echo "<br>";
+    }
+    ?>
+        
     <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>
     <script src='js/index.js'></script>
--- processData.php
+++ processData.php
@@ -6,6 +6,10 @@
     $name = $_POST['name'];
     $wins = $_POST['wins'];
     $losses = $_POST['losses'];
+    $yard = $_POST['yard'];
 
     $con = mysqli_connect($servername, $username, $password, $dbname);
 ?>
+<!--This sends the browser back to the home page-->
+<script>
+</script>
