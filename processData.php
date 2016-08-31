7faf7b1a4eb77c3df15f09b33979a3f6f899620f
falldj <derek.isensee@gmail.com>
2016-08-30 22:13:07 -0500

    fixed processData
    now sends browser back to index.php. also INSERT INTO SQL command works.

--- processData.php
+++ processData.php
@@ -9,7 +9,11 @@
     $yard = $_POST['yard'];
 
     $con = mysqli_connect($servername, $username, $password, $dbname);
+    mysqli_query($con, "INSERT INTO player (name, wins, losses, timesInYard) VALUES ('$name', '$wins', '$losses', '$yard')");
 ?>
 <!--This sends the browser back to the home page-->
 <script>
+    setTimeout(function() {
+       window.location.href='/dominoStats/index.php';
+    }, 1500);
 </script>
