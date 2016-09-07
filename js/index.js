1a0a83314ef9e56fa99614f35d37eb29408eb387
Derek Isensee <derek.isensee@gmail.com>
2016-09-07 08:21:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     //$("#dominoStatsHead").effect("bounce"); //testing for jquery animations, does WORK!
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        $("#date").attr("type", "date")
+        $("#date").attr("type", "date");
     }
     else
         $("#date").datepicker();
