cc02d4750097880d371c761e5254d8b02f2908dc
NeoTessla <neotessla@gmail.com>
2016-09-07 07:46:05 -0500

    fixes and starts
    fixed id problem and started on home page

--- processData.php
+++ processData.php
@@ -3,7 +3,7 @@
     $username = "root";
     $password = "";
     $dbname = "domino";
-	$id = Rand(0, 30);
+	
     //$firstName = $_POST['firstName'];
 	//$lastName = $_POST['lastName'];
     //$win = $_POST['win'];
@@ -22,7 +22,7 @@
 	$lScore = $_POST['lScore'];
 	
     $con = mysqli_connect($servername, $username, $password, $dbname);
-    mysqli_query($con, "INSERT INTO game (id, wFirst, wLast, lFirst, lLast, date, fDownLastName, wDrawTimes, wBones, wScore, lDrawTimes, lBones, lScore) VALUES ('$id', '$wFirst', '$wLast', '$lFirst', '$lLast', '$date', '$fDownLastName', '$wDrawTimes', '$wBones', '$wScore', '$lDrawTimes', '$lBones', '$lScore')");
+    mysqli_query($con, "INSERT INTO game (wFirst, wLast, lFirst, lLast, date, fDownLastName, wDrawTimes, wBones, wScore, lDrawTimes, lBones, lScore) VALUES ('$wFirst', '$wLast', '$lFirst', '$lLast', '$date', '$fDownLastName', '$wDrawTimes', '$wBones', '$wScore', '$lDrawTimes', '$lBones', '$lScore')");
 
     //$con = mysqli_connect($servername, $username, $password, $dbname);
     //mysqli_query($con, "INSERT INTO player (id, firstName, lastName, win, lost) VALUES ('$id', '$firstName','$lastName', '$win', '$lost')");
