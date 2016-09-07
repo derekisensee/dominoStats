6a616efdceca217c53e907977fbbf7aea35b8647
Derek Isensee - from school <derek.isensee@gmail.com>
2016-09-07 08:18:58 -0500

    mobile date picker

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     //$("#dominoStatsHead").effect("bounce"); //testing for jquery animations, does WORK!
-    $("#date").datepicker();
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        $("#date").attr("type", "date")
+    }
+    else
+        $("#date").datepicker();
 });
